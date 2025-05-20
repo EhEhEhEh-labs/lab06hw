c9b3b1b5e4052938dad520442adbee95cbe62707
EhEhEhEh-labs <new3now@yandex.ru>
2025-05-20 01:01:50 +0000

    Da budet svet

---
+++ build/formatter_ex_lib/CMakeFiles/formatter_ex_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../lib/libformatter_ex_lib.a"
+  "../lib/libformatter_ex_lib.pdb"
+  "CMakeFiles/formatter_ex_lib.dir/formatter_ex.cpp.o"
+  "CMakeFiles/formatter_ex_lib.dir/formatter_ex.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/formatter_ex_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/formatter_lib/CMakeFiles/formatter_lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "../lib/libformatter_lib.a"
+)
